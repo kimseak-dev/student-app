fc1d9c13229bd9cc99905707a7b23db8cbb186a0
Kimseak Dev <kimseak@kimseaks-MBP.mshome.net>
2017-05-19 10:48:58 +0700

    Maven Repository Inheritacen and Agregate

---
+++ student-ui/src/main/java/com/udemy/ui/App.java
@@ -0,0 +1,10 @@
+package com.udemy.ui;
+
+import com.udemy.util.Utils;
+
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println(Utils.GREETING);
+	}
+}
---
+++ student-util/src/main/java/com/udemy/util/Utils.java
@@ -0,0 +1,11 @@
+package com.udemy.util;
+
+public class Utils {
+	
+	private Utils(){
+		
+	}
+	
+	public static final String GREETING = "Hello world from multi module app!";
+
+}
